152be814d73cedf2c01ac6908e54512f29d6a967
xBlueAce <34463290+xBlueAce@users.noreply.github.com>
2019-09-23 16:35:57 -0700

    Update settings.py
    added bullet settings

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
-class Settings():
-    """A class to store all settings for Alien Invasion"""
+class Settings:
+    """A clas to store all settings for Alien Invasion."""
+
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
@@ -7,12 +8,12 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        # Ship settings.
+        # Ship settings
         self.ship_speed_factor = 1.5
 
         # Bullet settings.
         self.bullet_speed_factor = 1
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
