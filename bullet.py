56474c8b42fc3a18adbfbd781cf2f3278a167354
xBlueAce <34463290+xBlueAce@users.noreply.github.com>
2019-09-25 02:11:47 -0700

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,14 +1,15 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Bullet(Sprite):
-    """A ckass to manage bullets fired from the ship"""
+    """A class to manage bullets fired from the ship"""
 
     def __init__(self, ai_settings, screen, ship):
-        """ Create a bullet object at the ship's current position."""
+        """Create a bullet object at the ship's current position."""
         super(Bullet, self).__init__()
-        self.screen = screen # On page 258
-        
+        self.screen = screen
+
         # Create a bullet rect at (0,0) and then set correct position.
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
@@ -30,5 +31,4 @@ class Bullet(Sprite):
     def draw_bullet(self):
         """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
-
 
